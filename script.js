87491b12f5f83e6f226e3b4f5afc912ac3daedbf
Glitch (glitch-hello-website) <none>
2022-03-28 23:45:37 +0700

    Commit

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const Btn = document.getElementsByClassName("Btn");
+const Btn = document.getElementById("Btn");
 const True = document.querySelectorAll(".True");
 
 
 Btn.addEventListener("click", function () {
-    for (let i = 0, i < True.length, i++) {
+    for (let i = 0; i < True.length; i++) {
         True[i].checked = true;
     }
 })
